f5db4c462b33cd32637561e41a6a42e89669eb5f
soundx-code <soundxcode@gmail.com>
2022-06-04 23:58:36 +0100

    fixed the cancelling of task issue

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function addNewTask(e) {
         li.className = 'task-list-item'
 
         li.innerHTML = `<span>${newTask}</span>
-        <button class="delete">&#x2715;</button>
+        <button class="delete" onclick="delListItem(this)">&#x2715;</button>
         <button class="done">&check;</button>`
 
         document.getElementById('input-task').value = ""
@@ -38,9 +38,9 @@ for (var i = 0; i < dlt.length; i++) {
     dlt[i].addEventListener("click", delListItem)
 }
 
-function delListItem() {
+function delListItem(e) {
     console.log("here")
-    // this.parentNode.remove()
+    e.parentNode.remove()
 }
 
 
